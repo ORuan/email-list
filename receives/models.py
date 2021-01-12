eb839e3bed7e82dd43e08ee79083d788d9942847
ORuan <ruanpablomp@gmail.com>
2021-01-12 17:01:08 -0300

    create a basic project

---
+++ receives/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+
+
+# Create your models here.
+# For testingo-Dev
+# sudo docker run --name postgres -e POSTGRES_PASSWORD=posgres -d postgres
+
+class Campaign(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=40, null=False, unique=True)
+    start_data = models.DateTimeField(null=False)
+    end_data = models.DateTimeField(null=False)
+    hour_at_day = models.TimeField(null=True)
+    email_sender = models.EmailField(null=False)
+    created_at = models.DateTimeField(auto_created=timezone.now())
+
+class EmailsThatWillReceive(models.Model):
+    campaign_id = models.ForeignKey(Campaign, on_delete=models.CASCADE)
+    active = models.BooleanField(default=False)
+    subject = models.CharField(max_length=40, null=False)
+    name = models.CharField(max_length=40, null=False)
+    email = models.EmailField(null=False)
+    content = models.TextField(null=False)
+    created_at = models.DateTimeField(auto_created=timezone.now())
